7bd215bb53c74d1e68b892e055f8c063917cdba8
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-10-21 20:22:19 -0300

    updated logo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -16,8 +16,7 @@ const Footer: React.FC = () => {
           <div className="flex-h">
             <Button className='btn-text' href='https://etherscan.io/token/0xe1240ac7bb51333510cbf37efd678ca46137f84b' >RED CONTRACT</Button>
             <Button className='btn-text' href='https://etherscan.io/token/0x96B00208911d72eA9f10c3303fF319427A7884C9' >BLUE CONTRACT</Button>
-            <Button className='btn-text' href='' >UNISWAP</Button>
-            <Button className='btn-text' href='' >TWITTER</Button>
+            <Button className='btn-text' href='https://info.uniswap.org/pair/0x85256d98f126be819cb3a5395fdbf130f11fdd93' >UNISWAP</Button>
             <Button className='btn-text' href='https://t.me/DotsFinance' >TELEGRAM</Button>
           </div>
         </div>
