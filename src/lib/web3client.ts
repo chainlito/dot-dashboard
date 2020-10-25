3b0ba366968f779fb7e02e14e94ba582adcf77a0
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-10-25 05:28:52 -0300

    gas limit

--- src/lib/web3client.ts
+++ src/lib/web3client.ts
@@ -57,7 +57,7 @@ async function getTotalSupply(contract: any,): Promise<number> {
 }
 
 async function rebase(from: string) {
-  await orchestratorContract.methods.rebase().send({ from, gas: 300000 })
+  await orchestratorContract.methods.rebase().send({ from, gas: 450000 })
     .on('error', function(error: any, receipt: any) {
       console.log(error, receipt);
     });
